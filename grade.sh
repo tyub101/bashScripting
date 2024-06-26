05db894846d077b9729dcc785e5ca5acb728a5dd
ABDELILAH AIT AHMED OUHAMOU <96702843+tyub101@users.noreply.github.com>
2024-06-26 15:02:35 +0100

    Create grade.sh

---
+++ grade.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "enter your note : " 
+read -r note
+if [ "$note" -ge 16 ] ; then
+    echo "Trés Bien"
+elif [ "$note" -ge 14 ] ; then 
+    echo "Bien"
+elif [ "$note" -ge 12 ] ; then
+    echo "assez bien "
+elif [ "$note" -ge 10 ] ; then
+    echo "assez bien "
+else
+    echo "insuffisant"
+fi
