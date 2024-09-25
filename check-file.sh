08fc335ff9b5f632481ca3865cfca4d76ec7fb90
ABDELILAH AIT AHMED OUHAMOU <96702843+tyub101@users.noreply.github.com>
2024-09-25 14:53:48 +0100

    Create check-file.sh

---
+++ check-file.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+FILE="/etc/shadow"
+if [-e "$FILE"] 
+then
+ echo “Shadow passwords are enabled.”
+fi
+if [-w /etc/shadow ] 
+then
+ echo “You have permissions to edit /etc/shadow.”
+else 
+   echo “You do NOT have permissions to edit /etc/shadow.”
+fi
