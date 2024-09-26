c016a4a8c0f14ae31c3816d055268a5d797ce742
ABDELILAH AIT AHMED OUHAMOU <96702843+tyub101@users.noreply.github.com>
2024-09-26 14:18:55 +0100

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+function backup_file(){
+ if [ -f "$1" ]
+ then
+   local BACKUP_FILE="/var/tmp/$(basename ${1}).$(date +%F).$$”
+   echo " Backing up $1 to ${BACKUP_FILE}
+   cp $1 $BACKUP_FILE
+ fi
+}
+backup_file /etc/hosts
+ if [ "$0" -eq 0 ]
+ then 
+   echo " Backup Succeded !"
+fi
